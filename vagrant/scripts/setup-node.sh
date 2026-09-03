7cf809ee78e6c373893f55367fc72b082683844f
Gabriela Pires <178832712+gavvdev@users.noreply.github.com>
2026-09-03 15:10:52 -0300

    feat: setup-node desenvolvido

--- vagrant/scripts/setup-node.sh
+++ vagrant/scripts/setup-node.sh
@@ -1,2 +1,10 @@
-apt-get update
+apt-get update -y
+apt-get install -y openssh-server
 
+systemctl enable ssh
+systemctl start ssh
+
+mkdir -p /home/vagrant/.ssh
+
+chmod 700 /home/vagrant/.ssh
+chmod -R vagrant:vagrant /home/vagrant/.ssh
