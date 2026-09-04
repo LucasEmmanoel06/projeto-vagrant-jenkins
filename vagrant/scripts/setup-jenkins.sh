6c18d5828820924bfbe28beb64e234c0734f010d
gavvdev <gabrielappsilva1@outlook.com>
2026-09-04 23:30:34 +0000

    feat: adiciona script de instalação do Jenkins na VM host

---
+++ vagrant/scripts/setup-jenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y openjdk-17-jdk openssh-server wget gnupg
+
+wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
+  > /etc/apt/sources.list.d/jenkins.list
+
+apt-get update -y
+apt-get install -y jenkins
+
+systemctl enable jenkins
+systemctl start jenkins
+
+systemctl enable ssh
+systemctl start ssh
