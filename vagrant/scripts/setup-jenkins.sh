75a60f28eb6509a9428d5d9afdf59fda27e87fa6
LucasEmmanoel06 <lucas.albuquerque2k@gmail.com>
2026-09-05 13:20:40 -0300

    fix: chave do repositorio do jenkins atualizado para 2026

--- vagrant/scripts/setup-jenkins.sh
+++ vagrant/scripts/setup-jenkins.sh
@@ -4,7 +4,7 @@ apt-get update -y
 apt-get install -y openjdk-21-jdk openssh-server wget gnupg
 
 wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
   > /etc/apt/sources.list.d/jenkins.list
